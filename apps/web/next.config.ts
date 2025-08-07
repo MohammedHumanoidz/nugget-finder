515b632190c72073c1d83f1ab23f3cf31d1436c7
Mohammed Humanoidz <mohammed@humanoidz.ai>
2025-08-08 01:10:28 +0300

    feat: add web analytics

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -10,6 +10,8 @@ const nextConfig: NextConfig = {
 	experimental: {
 		typedRoutes: false,
 	},
+	// This is required to support PostHog trailing slash API requests
+	skipTrailingSlashRedirect: true,
 	async redirects() {
 		return [
 			{
@@ -22,6 +24,22 @@ const nextConfig: NextConfig = {
 						value: "www.nuggetfinder.ai",
 					},
 				],
+			},
+		];
+	},
+	async rewrites() {
+		return [
+			{
+				source: "/ingest/static/:path*",
+				destination: "https://us-assets.i.posthog.com/static/:path*",
+			},
+			{
+				source: "/ingest/:path*",
+				destination: "https://us.i.posthog.com/:path*",
+			},
+			{
+				source: "/ingest/flags",
+				destination: "https://us.i.posthog.com/flags",
 			},
 		];
 	},
